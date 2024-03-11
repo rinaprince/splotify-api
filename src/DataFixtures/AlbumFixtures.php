06e37f0925493ead7849adb473269cc72eeb0a95
rinaprince <marinacheca11@gmail.com>
2024-03-11 11:30:48 +0100

    Model de dades i dades d'exemple fet

--- src/DataFixtures/AlbumFixtures.php
+++ src/DataFixtures/AlbumFixtures.php
@@ -3,6 +3,8 @@
 namespace App\DataFixtures;
 
 use App\Entity\Album;
+use App\Entity\Song;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
@@ -13,7 +15,8 @@ class AlbumFixtures extends Fixture
     {
         $faker = Factory::create();
 
-        for ($i = 0; $i < 25; $i++) {
+        //Generar àlbums
+        for ($i = 0; $i <25; $i++) {
             $album = new Album();
             $album
                 ->setTitle($faker->sentence(3))
@@ -23,7 +26,33 @@ class AlbumFixtures extends Fixture
 
             $manager->persist($album);
             $this->addReference('album_' . ($i + 1), $album);
+
+            //Generar cançons per àlbum
+            $numSongs = rand(5, 15);
+            for ($j = 0; $j < $numSongs; $j++) {
+                $song = new Song();
+                $song
+                    ->setTitle($faker->unique()->sentence(3))
+                    ->setDuration(rand(120, 600))
+                    ->setAlbum($album);
+
+                $manager->persist($song);
+            }
         }
+
+        // Asignar àlbums a users
+        $admin = $manager->getRepository(User::class)->findOneBy(['username' => 'admin']);
+        $user = $manager->getRepository(User::class)->findOneBy(['username' => 'user']);
+        $albums = $manager->getRepository(Album::class)->findAll();
+
+        foreach ($albums as $album) {
+            if (rand(0, 1)) {
+                $admin->addLike($album);
+            } else {
+                $user->addLike($album);
+            }
+        }
+
 
         $manager->flush();
     }
