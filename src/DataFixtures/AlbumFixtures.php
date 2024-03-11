5bf4e58b4bba78f117afbb919b41c658b8d562ec
rinaprince <marinacheca11@gmail.com>
2024-03-11 10:38:07 +0100

    Fent AlbumFixtures

--- src/DataFixtures/AlbumFixtures.php
+++ src/DataFixtures/AlbumFixtures.php
@@ -2,16 +2,29 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Album;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AlbumFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create();
+
+        for ($i = 0; $i < 25; $i++) {
+            $album = new Album();
+            $album
+                ->setTitle($faker->sentence(3))
+                ->setReleasedAt($faker->dateTimeThisDecade())
+                ->setCover($faker->imageUrl())
+            ;
+
+            $manager->persist($album);
+        }
 
         $manager->flush();
+    }
     }
 }
